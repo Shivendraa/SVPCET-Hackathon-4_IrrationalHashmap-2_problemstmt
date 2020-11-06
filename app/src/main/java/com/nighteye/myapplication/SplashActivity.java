a0a44253d74e9b96591d6f335bece999eaf9bc16
Shivendraa <=>
2020-11-06 11:45:14 +0530

    Commit 1 - 11:45am 06-11-20

---
+++ app/src/main/java/com/nighteye/myapplication/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.nighteye.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView backgroundImage;
+    TextView sloganText;
+    Animation sideAnim, bottomAnim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_splash);
+
+        backgroundImage = findViewById(R.id.backgroundImg);
+        sloganText = findViewById(R.id.textSlogan);
+
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        backgroundImage.setAnimation(sideAnim);
+        sloganText.setAnimation(bottomAnim);
+
+        int SPLASH_TIMER = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
