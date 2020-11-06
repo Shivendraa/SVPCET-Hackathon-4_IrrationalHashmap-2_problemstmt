28f1cffba1e36ddc5d862a5de69cbb17e7eb8024
Shivendraa <=>
2020-11-06 23:56:31 +0530

    Mistake

--- app/src/main/java/com/nighteye/myapplication/UserRegisterNextStep.java
+++ app/src/main/java/com/nighteye/myapplication/UserRegisterNextStep.java
@@ -3,12 +3,17 @@ package com.nighteye.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class UserRegisterNextStep extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_userregister_nextstep);
+
+
+
     }
 }
